2216a38b2a3cd49518d1a53493b37c2edd4ba091
arena-abhishek <apdesign0001@gmail.com>
2024-09-01 16:22:35 +0530

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -230,6 +230,26 @@ let content2 = `<div class="container-xxl" style="background-color: #1c6644">
           </li>
         </ul>
       </div>
+      <div class="col-md-5 offset-md-1 mb-3 text-white">
+              <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                  <label for="newsletter1" class="visually-hidden"
+                    >Email address</label
+                  >
+                  <input
+                    id="newsletter1"
+                    type="text"
+                    class="form-control"
+                    placeholder="Email address"
+                  />
+                  <button class="subBtn" type="button">
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+            </div>
 
       <div
         class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top text-white"
@@ -255,4 +275,3 @@ let content2 = `<div class="container-xxl" style="background-color: #1c6644">
 </div>`;
 myFooter = makeElement("section", "container-fluid", "", "", content2);
 footer.append(myFooter);
-
